30ef7a9a6efed048f0e24ef16a50234b20b9d249
poznas <misterdannypl@gmail.com>
2019-02-07 21:12:44 +0100

    spring initializr

---
+++ src/main/java/com/bsm/oa/BsmOpenApiApplication.java
@@ -0,0 +1,14 @@
+package com.bsm.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsmOpenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsmOpenApiApplication.class, args);
+	}
+
+}
+
