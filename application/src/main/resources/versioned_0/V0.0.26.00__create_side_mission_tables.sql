768441de33e07d16cc5272c1fe9992391c60c029
poznas <misterdannypl@gmail.com>
2019-03-28 23:12:41 +0100

    create side mission related tables

---
+++ application/src/main/resources/versioned_0/V0.0.26.00__create_side_mission_tables.sql
@@ -0,0 +1,25 @@
+create table SIDE_MISSION_TYPE (
+  sm_type_id varchar(32) primary key,
+  description_document_url varchar(500),
+  equation varchar(500) not null,
+  dictionary_name varchar(128)
+);
+
+create table SIDE_MISSION_PROOF_REQ (
+  id serial primary key,
+  sm_type_id varchar(32) not null references SIDE_MISSION_TYPE(sm_type_id),
+  proof_req_type varchar(32) not null,
+  proof_example_url varchar(500),
+  hint_dict_key varchar(128)
+);
+
+create table SIDE_MISSION_PERFORM_PARAM (
+  sm_type_id varchar(32) not null references SIDE_MISSION_TYPE(sm_type_id),
+  equation_symbol varchar(8) not null,
+  perform_param_type varchar(24) not null,
+  to_judge_by varchar(16) not null check (to_judge_by in ('PROFESSOR', 'JUDGE')),
+  hint_dict_key varchar(128),
+  value_limit integer,
+  entries varchar(2400),
+  primary key (sm_type_id, equation_symbol)
+);
