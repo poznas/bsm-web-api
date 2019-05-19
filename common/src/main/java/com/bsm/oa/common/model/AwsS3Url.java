16d5eeae54e812b8e19c4241d92ed047df8ecc6a
poznas <misterdannypl@gmail.com>
2019-05-19 19:19:44 +0200

    fix S3 url validation pattern

--- common/src/main/java/com/bsm/oa/common/model/AwsS3Url.java
+++ common/src/main/java/com/bsm/oa/common/model/AwsS3Url.java
@@ -7,7 +7,10 @@ import lombok.Value;
 @Value
 public class AwsS3Url implements ValueObject<String> {
 
+  private static final String PATTERN
+    = "https://[a-z0-9][a-z0-9-.]*s3\\.[a-z0-9][a-z0-9-.]*\\.amazonaws\\.com/[a-z0-9][a-zA-Z0-9-.%/]*";
+
   @NotBlank
-  @Pattern(regexp = "https://s3\\.[a-z0-9][a-z0-9-.]*\\.amazonaws\\.com/[a-z0-9][a-z0-9-./]*")
+  @Pattern(regexp = PATTERN)
   private String value;
 }
